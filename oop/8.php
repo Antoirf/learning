0e8b5217b6fa8777ffe676a288f64181d61f3b7c
antoirf <aosovskoy@mail.ru>
2022-06-21 12:22:29 +0300

    complete task 1,2

---
+++ oop/8.php
@@ -0,0 +1,56 @@
+<?php
+//https://code.mu/ru/php/book/oop/getters-and-setters/
+class Employee{
+    private string $name;
+    private int $age;
+    private int $salary;
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return int
+     */
+    public function getAge(): int
+    {
+        return $this->age;
+    }
+
+    /**
+     * @param int $age
+     */
+    public function setAge(int $age): void
+    {
+        $this->age = $age;
+    }
+
+    /**
+     * @return int
+     */
+    public function getSalary(): int
+    {
+        return $this->salary;
+    }
+
+    /**
+     * @param int $salary
+     */
+    public function setSalary(int $salary): void
+    {
+        $this->salary = $salary;
+    }
+
+}
