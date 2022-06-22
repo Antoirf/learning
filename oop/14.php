45f6e1990d704995ea39d64a16f706a6d9f4f1ce
antoirf <aosovskoy@mail.ru>
2022-06-22 06:45:32 +0300

    complete task 2

--- oop/14.php
+++ oop/14.php
@@ -3,9 +3,9 @@
 //https://code.mu/ru/php/book/oop/variable-properties-names/
 class City
 {
-    private string $name;
-    private string $foundation;
-    private int $population;
+    public string $name;
+    public string $foundation;
+    public int $population;
 
     public function __construct($name, $foundation, $population)
     {
@@ -16,3 +16,7 @@ class City
 }
 
 $city = new City('spb', '1703', 8000000);
+$props = ['name', 'foundation', 'population'];
+foreach ($props as $prop) {
+    echo $city->$prop . PHP_EOL;
+}
