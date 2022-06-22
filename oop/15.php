a77e265a94a76296be2610e8265a6422d75bde8c
antoirf <aosovskoy@mail.ru>
2022-06-22 07:07:00 +0300

    complete task

---
+++ oop/15.php
@@ -0,0 +1,35 @@
+<?php
+
+class Friend
+{
+    private string $name;
+    private int $age;
+
+    public function __construct($name, $age)
+    {
+        $this->name = $name;
+        $this->age = $age;
+    }
+
+    /**
+     * @return int
+     */
+    public function getAge(): int
+    {
+        return $this->age;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+}
+
+$friend = new Friend('vasya', 50);
+$methods = ['method1' => 'getName', 'method2' => 'getAge'];
+echo $friend->{$methods['method1']}() . PHP_EOL;
+echo $friend->{$methods['method2']}();
