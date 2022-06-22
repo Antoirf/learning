ce7c4a11f71d912565c7ab1498a301389c8330b4
antoirf <aosovskoy@mail.ru>
2022-06-22 05:22:50 +0300

    complete task 2

--- oop/11.php
+++ oop/11.php
@@ -1,7 +1,22 @@
 <?php
+
 //https://code.mu/ru/php/book/oop/objects-in-array/
 class City
 {
     private string $name;
     public int $population;
+
+    public function __construct($name, $population)
+    {
+        $this->name = $name;
+        $this->population = $population;
+    }
 }
+
+$cityArray = [
+    new City('alupka', 500),
+    new City('voronezh', 100000),
+    new City('phuket', 5000),
+    new City('moscow', 20000000),
+    new City('city', 1)
+];
