93624e8ae2d50865c73f85e713488caf63d337fc
antoirf <aosovskoy@mail.ru>
2022-06-22 14:52:11 +0300

    complete task 1

---
+++ oop/19.php
@@ -0,0 +1,61 @@
+<?php
+
+//https://code.mu/ru/php/book/oop/classes-inheritance/
+class User
+{
+    private string $name;
+    private int $age;
+
+    /**
+     * @return int
+     */
+    public function getAge(): int
+    {
+        return $this->age;
+    }
+
+    /**
+     * @param int $age
+     */
+    public function setAge(int $age): void
+    {
+        $this->age = $age;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+}
+
+class Employee extends User
+{
+    private int $salary;
+
+    /**
+     * @return int
+     */
+    public function getSalary(): int
+    {
+        return $this->salary;
+    }
+
+    /**
+     * @param int $salary
+     */
+    public function setSalary(int $salary): void
+    {
+        $this->salary = $salary;
+    }
+}
