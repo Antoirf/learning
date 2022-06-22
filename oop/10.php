07332a270442330e6f6d26a6d7fbb820aa3ffbe6
antoirf <aosovskoy@mail.ru>
2022-06-22 12:40:55 +0300

    corrected

--- oop/10.php
+++ oop/10.php
@@ -1,6 +1,6 @@
 <?php
 //https://code.mu/ru/php/book/oop/one-class-one-file/
-require_once '9.php';
-$employee = new Employee3('john', 'smith',100);
 require_once '8.php';
+require_once '9.php';
+$employee = new Employee3('john', 'smith', 100);
 $employee2 = new Employee2();
