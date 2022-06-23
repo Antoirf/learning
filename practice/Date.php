82cf8aea03649d746060f958d9dfed6187d7eff6
antoirf <aosovskoy@mail.ru>
2022-06-23 13:15:16 +0300

    complete

--- practice/Date.php
+++ practice/Date.php
@@ -2,19 +2,21 @@
 
 class Date
 {
-    private string $date;
+    private string $timestamp;
 
     public function __construct($date = null)
     {
         if ($date === null) {
-            return $this->date = date('y-m-d');
+            $this->timestamp = strtotime(date('Y-m-d'));
+        } else {
+            $this->timestamp = strtotime($date);
         }
-        return $this->date = $date;
     }
+
 
     public function getDay()
     {
-        return date('d', strtotime($this->date));
+        return date('d', $this->timestamp);
     }
 
     public function getMonth($lang = null)
@@ -31,7 +33,7 @@ class Date
             'October' => 'Октябрь',
             'November' => 'Ноябрь',
             'December' => 'Декабрь'];
-        $month = date('F', strtotime($this->date));
+        $month = date('F', $this->timestamp);
         var_dump($month);
         if ($lang === 'en' or $lang === null) {
             return $month;
@@ -41,7 +43,7 @@ class Date
 
     public function getYear()
     {
-        return date('y', strtotime($this->date));
+        return date('Y', $this->timestamp);
     }
 
     public function getWeekDay($lang = null)
@@ -55,15 +57,56 @@ class Date
             'Saturday' => 'Суббота',
             'Sunday' => 'Воскресенье'
         ];
-        $weekDay = date('l', strtotime($this->date));
+        $weekDay = date('l', $this->timestamp);
         if ($lang === 'en' or $lang === null) {
             return $weekDay;
         }
         return $ruWeekDayArr[$weekDay];
     }
 
+    public function addDay($days)
+    {
+        $this->timestamp += $days * 60 * 60 * 24;
+    }
+
+    public function subDay($days)
+    {
+        $this->timestamp -= $days * 60 * 60 * 24;
+
+    }
+
+    public function addMonth($num)
+    {
+        $this->timestamp = strtotime('+' . $num . ' month', $this->timestamp);
+    }
+
+    public function subMonth($num)
+    {
+        $this->timestamp = strtotime('-' . $num . ' month', $this->timestamp);
+    }
+
+    public function addYear($num)
+    {
+        $this->timestamp = strtotime('+' . $num . ' year', $this->timestamp);
+    }
+
+    public function subYear($num)
+    {
+        $this->timestamp = strtotime('-' . $num . ' year', $this->timestamp);
+    }
+
+    public function __toString()
+    {
+        return date('Y-m-d', $this->timestamp);
+    }
+
+    public function format($format)
+    {
+        return date($format, $this->timestamp);
+    }
 }
 
 
 $date = new Date();
-print_r($date->getWeekDay('ru'));
+$date->subYear(2);
+echo $date;
