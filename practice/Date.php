b312417c0841f9b298bd76bb039a587110895cf1
antoirf <aosovskoy@mail.ru>
2022-06-23 12:34:06 +0300

    add russian month and week days

--- practice/Date.php
+++ practice/Date.php
@@ -17,16 +17,53 @@ class Date
         return date('d', strtotime($this->date));
     }
 
-    public function getMonth()
+    public function getMonth($lang = null)
     {
-        return date('F', strtotime($this->date));
+        $monthArr = ['January' => 'Январь',
+            'February' => 'Февраль',
+            'March' => 'Март',
+            'April' => 'Апрель',
+            'May' => 'Май',
+            'June' => 'Июнь',
+            'July' => 'Июль',
+            'August' => 'Август',
+            'September' => 'Сентябрь',
+            'October' => 'Октябрь',
+            'November' => 'Ноябрь',
+            'December' => 'Декабрь'];
+        $month = date('F', strtotime($this->date));
+        var_dump($month);
+        if ($lang === 'en' or $lang === null) {
+            return $month;
+        }
+        return $monthArr[$month];
     }
 
     public function getYear()
     {
         return date('y', strtotime($this->date));
     }
+
+    public function getWeekDay($lang = null)
+    {
+        $ruWeekDayArr = [
+            'Monday' => 'Понедельник',
+            'Tuesday' => 'Вторник',
+            'Wednesday' => 'Среда',
+            'Thursday' => 'Четверг',
+            'Friday' => 'Пятница',
+            'Saturday' => 'Суббота',
+            'Sunday' => 'Воскресенье'
+        ];
+        $weekDay = date('l', strtotime($this->date));
+        if ($lang === 'en' or $lang === null) {
+            return $weekDay;
+        }
+        return $ruWeekDayArr[$weekDay];
+    }
+
 }
 
+
 $date = new Date();
-print_r($date->getMonth());
+print_r($date->getWeekDay('ru'));
