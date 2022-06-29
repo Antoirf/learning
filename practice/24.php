b33937d254cbae9cf02fa856ffbee18d63c7ad19
antoirf <aosovskoy@mail.ru>
2022-06-29 09:57:07 +0300

    complete task 2,3,4,5

--- practice/24.php
+++ practice/24.php
@@ -7,6 +7,13 @@ class Product
     private int $quantity;
     private int $price;
 
+    public function __construct($name, $quantity, $price)
+    {
+        $this->name = $name;
+        $this->quantity = $quantity;
+        $this->price = $price;
+    }
+
     /**
      * @return string
      */
@@ -30,4 +37,28 @@ class Product
     {
         return $this->price;
     }
+
+    public function getCost()
+    {
+        return $this->getPrice() * $this->getQuantity();
+    }
 }
+
+class Cart
+{
+    private array $product;
+
+    public function add()
+    {
+        $this->product[] = new Product();
+    }
+
+    public function remove($name)
+    {
+        unset($this->product[$name]);
+    }
+
+}
+
+$product = new Product('orex', 2, 100);
+echo $product->getCost();
